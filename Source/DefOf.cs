8a3f0afe2ecc9ad062ca1fa5d620d55dab1a5c59
jhjjgu0115 <jhjjgu0115@naver.com>
2019-05-03 20:27:31 +0900

    publ

---
+++ Source/DefOf.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+
+
+namespace Lab
+{
+    [DefOf]
+    public static class JobDefOf
+    {
+        public static JobDef RK_Job_HamsterWheel;
+    }
+    [DefOf]
+    public static class WorkGiverDefOf
+    {
+        public static WorkGiverDef RK_Work_HamsterWheel;
+    }
+    [DefOf]
+    public static class ThingDefOf
+    {
+        public static ThingDef RK_HamsterWheelGenerator;
+    }
+    [DefOf]
+    public static class JoyKindDefOf
+    {
+        public static JoyKindDef Gaming_Dexterity;
+    }
+}
